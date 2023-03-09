7d66c9db292ed1e85e9001d04c6b18d723663b76
camaramamadou92 <71674569+camaramamadou92@users.noreply.github.com>
2023-03-09 19:30:40 +0100

    widget Vetement
    afficher une liste de vêtements filtrable par type et recherche, et permet aux utilisateurs de voir les détails de chaque vêtement en cliquant sur un bouton

---
+++ miaged/lib/VetementsMiaged/vetement.dart
@@ -0,0 +1,98 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+
+class MiagedVetements extends StatefulWidget {
+  const MiagedVetements(   { Key? key ,required this.categorieVetement}) : super(key: key);
+  final String? categorieVetement ;
+  @override
+  _MiagedVetementsState createState() => _MiagedVetementsState();
+}
+
+class _MiagedVetementsState extends State<MiagedVetements> {
+  TextEditingController checkerRecherche = TextEditingController();
+  // méthode qui sera appelée automatiquement au moment où l'état du widget sera initialisé
+  @override
+  void initState() {
+    checkerRecherche.addListener(() {
+      elementAffiche(); // appel de la méthode elementAffiche à chaque fois que le texte change dans le champ de recherche
+    });
+    super.initState();
+  }
+  // méthode qui permet de filtrer les éléments affichés selon le texte entré dans le champ de recherche
+  elementAffiche(){
+    setState(() {
+      checkerRecherche.text; // met à jour l'état de l'élément en utilisant le texte du champ de recherche
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    const TextStyle textStyle = TextStyle(fontSize: 14);
+    // création d'un style de bouton pour les boutons "Détails"
+    final ButtonStyle buttonStyle = ElevatedButton.styleFrom(textStyle:textStyle,
+        primary: const Color(0xFFCD5C5C));
+    // construction de la vue
+    return Column(children: [
+      // champ de recherche
+      TextFormField(
+        textAlignVertical: TextAlignVertical.center,
+        controller: checkerRecherche,
+        decoration: const InputDecoration(
+
+          prefixIcon: Icon(Icons.search),
+          hintText: "Rechercher un vêtement",
+          contentPadding: EdgeInsets.only(left: 24.0),
+          border: InputBorder.none,
+        ),
+      ),
+      // liste des vêtements
+      Expanded(
+          child :StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('Habits').where('Type', isEqualTo: widget.categorieVetement)
+                  .snapshots(),
+              builder : (context, AsyncSnapshot<QuerySnapshot> snapshot){
+                if (!snapshot.hasData) {
+                  return const Center(child : Text('Loading'));
+                }
+                return ListView(children :
+                // mapping des éléments de la liste Firestore pour les afficher dans un Card
+                snapshot.data!.docs.map((QueryDocumentSnapshot<Object?> item ) {
+                  if(item['Titre'].toString().trim().toLowerCase().contains(checkerRecherche.text.trim().toLowerCase())){
+                    return Center(
+                        child: Card(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              ListTile(
+                                leading: Image.network(item['Photo']),
+                                title: Text(item['Titre']),
+                                subtitle: Text('Taille : '+item['Taille'] + '\nPrix : ' + item['Prix'].toString() + ' €'  ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: <Widget>[
+
+                                  const SizedBox(width: 8),
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              )
+                            ],
+                          ),
+                        )
+                    );
+                  }
+                  else{
+                    return const SizedBox(height: 0);
+                  }
+                } ).toList()
+                );
+              }
+          ))]);
+
+  }
+
+
+}
